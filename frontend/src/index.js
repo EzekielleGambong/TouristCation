8428112a8c07569b7274fbacd0052711e6ec79e5
lorenzjedd <lorenzjeddalvarez2@gmail.com>
2024-10-12 01:49:48 +0800

    # UPDATE
    + added setup for designing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // # ~ ~ ~ ~ ~ # # ~ ~ ~ ~ ~ # CSS # ~ ~ ~ ~ ~ # # ~ ~ ~ ~ ~ # //
 import './index.css';
@@ -8,8 +9,30 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+export default function TouristCation() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />}/>
+
+        {/* SAMPLE */}
+        {/* <Route path="/user/" element={<LayoutUser />}>
+          <Route path="/user/packages" element={<Packages />} />
+          <Route path="/user/new_client" element={<NewClient />} />
+          <Route path="/user/client_status" element={<ClientStatus />} />
+          <Route path="/user/reports" element={<Reports />} />
+          <Route path="/user/client_history" element={<ClientHistory />} />
+
+          <Route index element={<Error />} />
+          <Route path="/user/*" element={<Error />} />
+        </Route> */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    <TouristCation />
   </React.StrictMode>
 );
