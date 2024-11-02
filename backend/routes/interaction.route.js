98d63d61612567a894c7628ae0cdaef55e3d623a
Ezekielle Louise Marie L. Gambong <104586559+EzekielleGambong@users.noreply.github.com>
2024-11-02 15:18:16 +0800

    draft VER 2

--- backend/routes/interaction.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const { postDogPreference, postCatPreference } = require('../controllers/interaction.controller')
-const router = express.Router()
-
-router.post('/dog-preference', postDogPreference)
-router.post('/cat-preference', postCatPreference)
-
-module.exports = router
