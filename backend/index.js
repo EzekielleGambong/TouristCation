c2d613773f47833f526a0cd021c9c5b0c4e04b82
Ezekielle Louise Marie L. Gambong <ezegambong@gmail.com>
2024-11-07 04:47:22 +0800

    draft4

--- backend/index.js
+++ backend/index.js
@@ -10,25 +10,12 @@ app.get('/', (req, res) => {
     res.send('Try')
 })
 
-const userRoutes = require('./routes/user.router')
-const listingsUploadRoutes = require('./routes/listingsUpload.router')
-const userprofileRoutes = require('./routes/userprofile.router')
-const ListingsUpload = require('./models/listingsUpload.model')
-const adoptercatpreferences = require('./routes/adoptercatpreferences.router')
-const adopterdogpreferences = require('./routes/adopterdogpreferences.router')
-const dogpetinformation = require('./routes/dogpetinformation.router')
-const catpetinformation = require('./routes/catpetinformation.router')
-const interactionRoute = require('./routes/interaction.route')
-const adoptionapplication = require('./routes/adoptionapplication.router')
-const questionnaire = require('./routes/questionnaire.router')
+// const userRoutes = require('./routes/user.router')
+// // Assuming the User model is in './models/user.model'
+// const User = require('./models/user.model');
 
-// Assuming the User model is in './models/user.model'
-const User = require('./models/user.model');
-const DogPetInformation = require('./models/dogpetinformation.model');
-const CatPetInformation = require('./models/catpetinformation.model');
-const AdoptionApplication = require('./models/adoptionapplication.model');
 
-app.use('/users', userRoutes)
+// app.use('/users', userRoutes)
 
 
 mongoose.connect(process.env.CONNECTION_STRING,
