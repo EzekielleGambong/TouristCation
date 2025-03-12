a422c5ab4cf87419afbb7763b78c52a55f705e4b
Ezekielle Louise Marie L. Gambong <ezegambong@gmail.com>
2025-03-13 07:53:05 +0800

    Before email

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:8080" });
+const API = axios.create({ baseURL: "https://www.toursitcation.site" });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
